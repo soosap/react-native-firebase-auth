e67a4109ecd7d287b68cffe83affd99ac4a2a51d
soosap <prasath.soosaithasan@gmail.com>
2016-09-24 15:33:11 +0200

    Add basic directory structure and register <App /> with rn's AppRegistry

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-native-firebase
+### $ git clone https://github.com/soosap/react-native-firebase.git FirebaseAuth
+
+### $ npm install && react-native run-ios
+
+<br />
+
+![Demo](src/images/demo.gif?raw=true "FirebaseAuth")
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Hello</Text>
+			</View>
+		);
+	}
+}
+
+export default App;
