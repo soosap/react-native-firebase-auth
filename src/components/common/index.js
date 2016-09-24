39ee39cec1096c83de6b6dfeeab04ba2f6a181e8
soosap <prasath.soosaithasan@gmail.com>
2016-09-24 15:49:39 +0200

    Refactor common components into "src/components/common"

---
+++ src/components/common/index.js
@@ -0,0 +1,11 @@
+import Button from './Button';
+import Card from './Card';
+import CardSection from './CardSection';
+import Header from './Header';
+
+export default {
+	Button,
+	Card,
+	CardSection,
+	Header,	
+};
