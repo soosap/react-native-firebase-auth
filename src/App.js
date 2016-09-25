93742832fb18becdf40de72e0c399bb974a1c1e9
soosap <prasath.soosaithasan@gmail.com>
2016-09-25 14:59:36 +0200

    Initialize Firebase w/ config props taken from the console

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
 	componentDidMount() {
 		this.setState({ firebaseRef: new Firebase('https://react-native-fbauth.firebaseio.com/') });
+	componentWillMount() {
+		Firebase.initializeApp({
+			apiKey: 'AIzaSyCXt5Pd_XOoA6VAGoX4lIK-bsZXQ5vXNXQ',
+			authDomain: 'react-native-fbauth.firebaseapp.com',
+			databaseURL: 'https://react-native-fbauth.firebaseio.com',
+			storageBucket: 'react-native-fbauth.appspot.com',
+			messagingSenderId: '345393354397',
+		});
+
+		Firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.setState({ isLoggedIn: true });
+			} else {
+				this.setState({ isLoggedIn: false });
+			}
+		});
 	}
 
 	render() {
